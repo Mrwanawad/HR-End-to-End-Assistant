4f63bae3d64753b508f78f665cb5d7e86c991bb0
mrwanawad <mrwaanawad@gmail.com>
2026-05-20 04:41:12 +0200

    Editing config.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,8 +1,7 @@
-import os
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from colorama import Fore, Back
-from typing import List
 from pathlib import Path
+from typing import List
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from colorama import Fore
 
 ENV_PATH = Path(__file__).resolve().parent.parent.parent / ".env"
 
@@ -12,30 +11,25 @@ class Settings(BaseSettings):
 
     LLM_PROVIDER: str
 
-    IMAGE_MAX_SIZE:       int
-    IMAGES_ALLOWED_TYPES: List[str]
+    IMAGE_MAX_SIZE:       int = 10
+    IMAGES_ALLOWED_TYPES: List[str] = ["image/png", "image/jpg", "image/jpeg"]
 
-    FILE_MAX_SIZE:        int
-    FILE_ALLOWED_TYPES:   List[str]
+    FILE_MAX_SIZE:        int = 100
+    FILE_ALLOWED_TYPES:   List[str] = ["text/plain", "application/pdf", "application/docx"]
 
     MISTRAL_MODEL:   str
     MISTRAL_API_KEY: str
-    
-    GROQ_MODEL: str
+
+    GROQ_MODEL:   str
     GROQ_API_KEY: str
 
-    OLLAMA_MODEL:    str
+    OLLAMA_MODEL: str
 
     model_config = SettingsConfigDict(
-    env_file=ENV_PATH if ENV_PATH.exists() else None,    # For Streamlit Cloud Deployment
-    env_file_encoding="utf-8"
+        env_file=ENV_PATH if ENV_PATH.exists() else None,
+        env_file_encoding="utf-8"
     )
 
 
 def get_settings():
     return Settings() # type: ignore
-
-
-if __name__ == "__main__":
-    print(  Fore.RED + f'Mistral API_KEY: { get_settings().MISTRAL_API_KEY } ' + Fore.RESET )
-    
